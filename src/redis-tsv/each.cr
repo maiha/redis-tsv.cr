219c93db381ba477ce8eaf4c0fe1f1996e936042
maiha <maiha@wota.jp>
2016-06-12 02:43:40 +0900

    each

---
+++ src/redis-tsv/each.cr
@@ -0,0 +1,39 @@
+require "redis"
+
+class Redis
+  module Commands
+    # EACH has the same effect and sematic as SCAN,
+    # but it is easy to use with block.
+    #
+    # **Options**:
+    # see SCAN
+    #
+    # **Return value**: Nil
+    #
+    # Example:
+    #
+    # ```
+    # redis.each { |key| p key }
+    # redis.each(match: "foo:*") { |key| p key }
+    # redis.each(count: 100) { |key| p key }
+    # ```
+    
+    def each(match = "*", count = nil)
+      idx = 0
+      while true
+        idx, keys = scan(idx, match, count)
+        unless idx.is_a?(String)
+          raise "scan failed due to invalid idx: expected String but got `#{idx.class}'"
+        end
+        idx = idx.to_i
+        unless keys.is_a?(Array)
+          raise "scan failed due to invalid keys: expected Array but got `#{keys.class}'"
+        end
+        keys.each do |key|
+          yield key.to_s
+        end
+        break if idx == 0
+      end        
+    end
+  end
+end
